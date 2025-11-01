f249a7e0158fb9c3bfdb2a4b36ec5fcb5d402850
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-01 23:21:04 +0530

    update

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "igw_tags" {
 }
 
 variable "public_subnet_cidrs" {
-  type = list(any)
+  type = list
   validation {
     condition     = length(var.public_subnet_cidrs) == 2
     error_message = "Please provide 2 valid public subnet CIDR"
